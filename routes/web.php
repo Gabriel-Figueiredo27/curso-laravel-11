90b07175b427b6619e8306b1194a1201ab85c02e
gabriel <gabriel.guedes@edu.montesclaros.mg.gov.br>
2026-01-23 15:41:00 -0300

    Projeto Laravel  - CRUD de Usuários 2.0

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,24 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+//ROTAS DE USERS
+
+Route::get('/usuarios', [UserController::class, 'index']) ->name('users.index');
+
+Route::get('/usuarios/cadastro', [UserController::class, 'create']) ->name('users.create');
+
+Route::post('/usuarios/cadastro', [UserController::class, 'store']) ->name('users.store');
+
+Route::post('/usuarios/editar', [UserController::class, 'edit']) ->name('users.edit');
+
+Route::get('/usuarios/mostrar/{id}', [UserController::class, 'show'])->name('users.show');
+
+Route::post('/usuarios/editar/{id}', [UserController::class, 'update'])->name('users.update');
+
+//FIM
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
